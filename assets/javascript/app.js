5ce98f7b5d4bba44c7103d16a3d1fcef43ae0825
Neil Froggatt <neilfr@live.com>
2018-10-26 19:50:54 -0400

    added the incorrect and correct answer screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,8 @@
-var rights=0;
-var wrongs=0;
-var unanswered=0;
-var gameState = "start";
-var questionNumber=0;
+
+var rights;
+var wrongs;
+var unanswered;
+var questionNumber;
 
 // load question bank
 var trivia =[
@@ -30,6 +30,7 @@ var trivia =[
     }
 ];
 
+// setup the start screen
 function startScreen(){
     $('.startScreen').show();
     $('.questionScreen').hide();
@@ -37,20 +38,27 @@ function startScreen(){
     $('.statScreen').hide();
 }
 
+// setup the question screen
 function questionScreen(){
+    questionNumber=0;
+    rights=0;
+    wrongs=0;
+    unanswered=0;
     $('.startScreen').hide();
     $('.questionScreen').show();
     $('.answerScreen').hide();
     $('.statScreen').hide();
 }
 
+// setup the answer screen
 function answerScreen(){
     $('.startScreen').hide();
     $('.questionScreen').hide();
-    $('.answerScreen').hide();
-    $('.statScreen').show();
+    $('.answerScreen').show();
+    $('.statScreen').hide();
 }
 
+// setup the end of game statistics screen
 function statScreen(){
     $('.startScreen').hide();
     $('.questionScreen').hide();
@@ -69,15 +77,37 @@ function askQuestion(number){
     $('#D').html(trivia[number].optionD);
 }
 
-// checks if answer is right or wrong and increments appropriate counter
+// check if the answer is right or wrong and increment the appropriate counter
 function checkAnswer(number){
     if(event.target.id===trivia[number].answer){
         rights++;
+        $('#answerStatus').html("You are correct!");
         console.log("you were right: "+rights);
     }else{
         wrongs++;
+        $('#answerStatus').html("Unfortunately, you are incorrect...");
+        switch (trivia[number].answer){
+            case 'A':
+                console.log("i got in!");
+                $('#correctAnswer').html("The correct answer is..."+trivia[number].optionA);
+                break;
+            case 'B':
+                console.log("got here too");
+                $('#correctAnswer').html("The correct answer is..."+trivia[number].optionB);
+                break;
+            case 'C':
+                $('#correctAnswer').html("The correct answer is..."+trivia[number].optionC);
+            break;
+            case 'D':
+                $('#correctAnswer').html("The correct answer is..."+trivia[number].optionD);
+            break;
+            default:
+                console.log("something went wrong");
+        }
+
         console.log("you were wrong: "+wrongs);
     }
+    answerScreen();
 }
 
 // begin the game on the startScreen
@@ -89,24 +119,18 @@ $('#startButton').on('click', function(){
     askQuestion(questionNumber);
 });
 
-// check answer and ask next question until the end of the question bank
+// check the if the selected answer is correct and then ask the next question... until the end of the question bank
 $('.answer').on('click', function(){
-    console.log("question number: "+questionNumber);
-    console.log("trivia.length is: "+trivia.length);
-    console.log("you clicked answer: "+event.target.id);
     checkAnswer(questionNumber);
     if (++questionNumber<trivia.length){
         askQuestion(questionNumber);
     }else{
-        gameState="done";
         statScreen();
     }
 });
 
+// start the game over again by going to the questionScreen
 $('#startOverButton').on('click', function(){
-    questionNumber=0;
-    rights=0;
-    wrongs=0;
     questionScreen();
 });
 
